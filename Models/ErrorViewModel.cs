3a97864f93dd5f9c536962f702b2c1d9a6679a4d
sandeep084991904 <sandeep084991904@gmail.com>
2024-07-28 12:32:39 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPNETCORE_Demo_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
